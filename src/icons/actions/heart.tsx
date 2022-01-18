192d88b06b62c3e52c29c556330cff558bfd452e
Denis <felon_91@mail.ru>
2022-01-18 20:51:52 +0300

    refactor: 💡 add viewbox for heart icon

--- src/icons/actions/heart.tsx
+++ src/icons/actions/heart.tsx
@@ -6,7 +6,7 @@ const Heart = (property: IconProps): ReturnType<React.FC> => {
   const props = castIconProps(property);
 
   return (
-    <svg {...props}>
+    <svg viewBox="0 0 24 18" {...props}>
       <path
         d="M21.845 2.257C18.591-1.676 12 4.878 12 4.878S5.408-1.676 2.155 2.257C-3.238 8.777 12 19 12 19S27.238 8.776 21.845 2.257Z"
         strokeWidth={2}
