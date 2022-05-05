bf84834ab5e15538059dcf19762eab818cc50c5c
Petr Pazhitnykh <pazhitnykhpetr@gmail.com>
2022-05-05 18:15:10 +0200

    fix: 🐛 fix a1 icon

--- src/icons/object/card/number-a1.tsx
+++ src/icons/object/card/number-a1.tsx
@@ -6,31 +6,34 @@ const NumberA1 = (property: IconProps): ReturnType<React.FC> => {
   const props = castIconProps(property);
 
   return (
-    <svg viewBox="0 0 17 16" {...props}>
-      <g clipPath="url(#yvp-a)">
-        <path d="m16 .5-.5-.5v10.28l.5-.31V.5zm-3.8 4.54-.87-.12a12.12 12.12 0 0 0 2.04-1.16v.68c-.37.24-.76.45-1.16.62l-.01-.02z" />
+    <svg fill="none" {...props}>
+      <g clipPath="url(#csi-a)">
         <path
-          d="M13.72.24a4.3 4.3 0 0 1-2.4 2.6v2.08a12.12 12.12 0 0 0 2.04-1.16v6.34l2.16.19V0l-1.79.24z"
-          fill="url(#yvp-b)"
+          d="M16.003.496 15.507 0v10.28l.496-.312V.496ZM12.187 5.04l-.864-.12c.716-.32 1.399-.708 2.04-1.16v.68c-.37.241-.76.447-1.168.616l-.008-.016Z"
+          fill="#000"
         />
         <path
-          d="m8.99 14.77.92.31-.74-2.58-.83.04.65 2.23zm-1.36-4.61L6.8 6.99l.47-1.52 1.29 4.67h-.93v.02z"
+          d="M13.723.24a4.306 4.306 0 0 1-2.4 2.592V4.92c.716-.32 1.399-.708 2.04-1.16v6.344l2.16.184V0L13.73.24h-.008Z"
+          fill="url(#csi-b)"
+        />
+        <path
+          d="m8.987 14.768.92.312-.736-2.584-.832.048.648 2.224ZM7.635 10.16l-.832-3.168.464-1.52 1.296 4.688h-.928Z"
           fill="#790000"
         />
-        <path d="m4.56 2.34 1.16-.62L.8 16l-.8-.56 4.56-13.1z" fill="#B40302" />
+        <path d="m4.563 2.336 1.16-.616L.803 16l-.8-.56 4.56-13.096v-.008Z" fill="#B40302" />
         <path
           fillRule="evenodd"
           clipRule="evenodd"
-          d="M.8 16 5.72 1.72l3.02.56 3.76 12.49-2.6.31-.73-2.58-4 .24-.86 2.9L.8 16zM7.27 5.48l1.29 4.67H5.84l1.42-4.67z"
-          fill="url(#yvp-c)"
+          d="m.803 16 4.92-14.28 3.016.56 3.76 12.488-2.592.312-.736-2.584-4 .24-.864 2.896L.803 16ZM7.267 5.48l1.288 4.672H5.85L7.267 5.48Z"
+          fill="url(#csi-c)"
         />
       </g>
       <defs>
         <linearGradient
-          id="yvp-b"
-          x1={13.42}
+          id="csi-b"
+          x1={13.419}
           y1={0}
-          x2={13.42}
+          x2={13.419}
           y2={10.28}
           gradientUnits="userSpaceOnUse"
         >
@@ -38,18 +41,18 @@ const NumberA1 = (property: IconProps): ReturnType<React.FC> => {
           <stop offset={1} stopColor="#141414" />
         </linearGradient>
         <linearGradient
-          id="yvp-c"
-          x1={3.63}
-          y1={5.42}
-          x2={11.27}
+          id="csi-c"
+          x1={3.635}
+          y1={5.416}
+          x2={11.267}
           y2={15.2}
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="#FA0100" />
           <stop offset={1} stopColor="#9B0000" />
         </linearGradient>
-        <clipPath id="yvp-a">
-          <path fill="#fff" d="M0 0h16v16H0z" />
+        <clipPath id="csi-a">
+          <path fill="#fff" transform="translate(.003)" d="M0 0h16v16H0z" />
         </clipPath>
       </defs>
     </svg>
