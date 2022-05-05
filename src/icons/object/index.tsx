ff63558beb25edea229f74c9c5f3131788bd4c39
Petr Pazhitnykh <pazhitnykhpetr@gmail.com>
2022-05-05 18:27:49 +0200

    feat: 🎸 add district and region icons

--- src/icons/object/index.tsx
+++ src/icons/object/index.tsx
@@ -1,10 +1,10 @@
 export { default as AddressIcon } from './address';
 export { default as AreaIcon } from './area';
+export { default as DistrictIcon } from './district';
 export { default as MapPinIcon } from './map-pin';
 export { default as MetroIcon } from './metro';
 export { default as ParametersIcon } from './parameters';
 export { default as PhotosIcon } from './photos';
 export { default as PriceIcon } from './price';
+export { default as RegionIcon } from './region';
 export { default as StreetIcon } from './street';
-export * from './card';
-export * from './search';
