4b75466b86809bb36fcb2997a6c37dcf57deb589
Petr Pazhitnykh <pazhitnykhpetr@gmail.com>
2022-05-23 11:29:34 +0200

    fix: 🐛 fix a1 icon

--- src/stories/render.tsx
+++ src/stories/render.tsx
@@ -1,10 +1,30 @@
 import type { FC } from 'react';
+import { useState } from 'react';
 
 import type { IconProps } from '../core';
 
-const renderIconsStory = (icons: Record<string, FC<IconProps>>) => () =>
-  (
-    <div className="page-wrapper">
+const renderIconsStory = (icons: Record<string, FC<IconProps>>) => () => {
+  const [isFill, setIsFill] = useState(false);
+
+  return (
+    <div
+      title={isFill ? 'remove fill' : 'add fill'}
+      className="page-wrapper"
+      onClick={() => {
+        const wrapper = document.querySelector(`#wrapper`);
+
+        if (isFill) {
+          wrapper?.classList.remove('icon-fill');
+          setIsFill(!isFill);
+          return;
+        }
+
+        wrapper?.classList.add('icon-fill');
+        setIsFill(!isFill);
+      }}
+      role="presentation"
+      id="wrapper"
+    >
       {Object.entries(icons).map(([name, Icon]) => (
         <div key={name} className="icon-wrapper">
           <Icon size={50} fill={false} />
@@ -13,5 +33,6 @@ const renderIconsStory = (icons: Record<string, FC<IconProps>>) => () =>
       ))}
     </div>
   );
+};
 
 export default renderIconsStory;
