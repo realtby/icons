8b489c7a89433934a4db2adcd97ca5b7771f8bca
e-egor <e.egorov@realt.by>
2022-09-27 10:06:46 +0300

    fix: 🐛 fix close icon view box

--- src/icons/misc/close.tsx
+++ src/icons/misc/close.tsx
@@ -6,7 +6,7 @@ const Close = (property: IconProps): ReturnType<React.FC> => {
   const props = castIconProps(property);
 
   return (
-    <svg viewBox="0 0 11 10" fill="none" {...props}>
+    <svg viewBox="0 0 10 10" fill="none" {...props}>
       <path d="M9.759 1.423A.833.833 0 1 0 8.58.244L5.003 3.821 1.426.244A.833.833 0 1 0 .247 1.423L3.824 5 .247 8.577a.833.833 0 1 0 1.179 1.179l3.577-3.577L8.58 9.756A.833.833 0 1 0 9.76 8.577L6.18 5 9.76 1.423Z" />
     </svg>
   );
