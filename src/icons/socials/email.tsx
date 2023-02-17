0e68d576b27a32dbd122562504da137c4477a6f3
Egor Glushanko <e.glushanko@realt.by>
2023-02-17 13:39:18 +0300

    fix: 🐛 redesign Site, Email, Instagram, InstagramNoFill icons

--- src/icons/socials/email.tsx
+++ src/icons/socials/email.tsx
@@ -2,19 +2,19 @@ import * as React from 'react';
 import type { IconProps } from '../../core';
 import { castIconProps } from '../../core';
 
-const Email = (property: IconProps): ReturnType<React.FC> => {
+const EmailW = (property: IconProps): ReturnType<React.FC> => {
   const props = castIconProps(property);
 
   return (
-    <svg viewBox="0 0 25 25" {...props}>
-      <path d="m16.03 13.42-2 1.92c-.82.77-2.26.79-3.09 0l-2.01-1.92-7.22 6.87c.27.12.57.19.88.19h19.78c.32 0 .61-.07.88-.19l-7.22-6.87z" />
-      <path d="M22.37 4.48H2.6c-.32 0-.61.07-.88.19L9.42 12l2.52 2.38c.26.26.82.26 1.09 0l2.5-2.38 7.72-7.34a2.18 2.18 0 0 0-.88-.2zM.7 5.6a1.9 1.9 0 0 0-.22.88v12a2 2 0 0 0 .22.88l7.24-6.88L.7 5.6zm23.56 0-7.24 6.88 7.24 6.88c.14-.27.22-.56.22-.88v-12c0-.32-.08-.61-.22-.88z" />
+    <svg viewBox="0 0 22 22" {...props}>
+      <path d="m14.258 11.865-1.842 1.753c-.748.711-2.062.727-2.825 0l-1.843-1.753-6.618 6.296c.247.108.518.172.807.172h18.132a2 2 0 0 0 .807-.172l-6.618-6.296Z" />
+      <path d="M20.07 3.667H1.936c-.289 0-.56.064-.807.172l7.072 6.728h.001v.002l2.3 2.187c.245.232.756.232 1 0l2.299-2.187.001-.002h.002l7.07-6.728a2 2 0 0 0-.806-.172ZM.209 4.693a1.736 1.736 0 0 0-.206.806v11c0 .291.078.563.206.807L6.838 11 .208 4.693ZM21.797 4.693 15.168 11l6.63 6.306c.127-.244.205-.516.205-.806v-11c0-.292-.078-.563-.206-.807Z" />
     </svg>
   );
 };
 
-Email.defaultProps = {
-  height: 16,
-  width: 16,
+EmailW.defaultProps = {
+  height: 22,
+  width: 22,
 };
-export default Email;
+export default EmailW;
