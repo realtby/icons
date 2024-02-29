957f74e9a73f7b8066902b547bb3c923349ecad6
Raman Tsikhanovich <r.tikhanovich@realt.by>
2024-02-29 16:17:14 +0300

    feat: 🎸 hide competitors icon

--- src/icons/services/index.tsx
+++ src/icons/services/index.tsx
@@ -2,3 +2,5 @@ export { default as CopyOfferBrandIcon } from './copy-offer-brand';
 export { default as CopyOfferIcon } from './copy-offer';
 export { default as ExchangeBrandIcon } from './exchange-brand';
 export { default as ExchangeIcon } from './exchange';
+export { default as HideCompetitorsBrandIcon } from './hide-competitors-brand';
+export { default as HideCompetitorsIcon } from './hide-competitors';
