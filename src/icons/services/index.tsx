b2f4cc3ed90d9882560b362adb288fccb6f61bf7
Raman Tsikhanovich <r.tikhanovich@realt.by>
2024-03-18 11:50:01 +0300

    feat: 🎸 recommend offer icon

--- src/icons/services/index.tsx
+++ src/icons/services/index.tsx
@@ -4,3 +4,5 @@ export { default as ExchangeBrandIcon } from './exchange-brand';
 export { default as ExchangeIcon } from './exchange';
 export { default as HideCompetitorsBrandIcon } from './hide-competitors-brand';
 export { default as HideCompetitorsIcon } from './hide-competitors';
+export { default as RecommendOfferBrandIcon } from './recommend-offer-brand';
+export { default as RecommendOfferIcon } from './recommend-offer';
